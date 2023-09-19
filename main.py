a9dae851315548f4e45ecab7b36a6e8f846042e1
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2023-09-18 21:06:49 -0400

    refactor: improve plots by using actual scale values on x-axis

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ MAX_SEARCH_AREA_RATIO = 0.1
 BLUR_KERNEL_SIZE = 21
 GOOD_MATCH_RATIO = 0.7
 SCALE_MAX_Y = 260
+SCALE_MAX_Y_VALUE = 0
 SCALE_MIN_Y = 890
+SCALE_MIN_Y_VALUE = 10
 STRIP_START_X = 350
 STRIP_END_X = 420
 USE_MEDIAN = False
@@ -26,7 +28,7 @@ def next_axs():
     return ax
 
 
-def calculate_tds(input, reference, show_plots=True, write_images=True, debug_enabled=True):
+def calculate_value(input, reference, show_plots=True, write_images=True, debug_enabled=True):
     def debug(*args):
         if debug_enabled:
             print(*args)
@@ -91,7 +93,8 @@ def calculate_tds(input, reference, show_plots=True, write_images=True, debug_en
     if show_plots:
         test_strip = next_axs()
         test_strip.set_title('Raw Strip')
-        test_strip.imshow(np.rot90(img4, 1))
+        test_strip.imshow(np.rot90(img4, 1), extent=[SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, 0, img4.shape[0]],
+                          aspect='auto')
 
     # average out the whole strip
     if USE_MEDIAN:
@@ -100,11 +103,35 @@ def calculate_tds(input, reference, show_plots=True, write_images=True, debug_en
         img4 = np.average(img4, axis=1)
     plot = img4.flatten()
 
+    def convert_index_to_ratio(index, invert=False):
+        ratio = index / len(plot)
+
+        if invert:
+            return 1 - ratio
+        else:
+            return ratio
+
+    # currently we are talking about salinity
+    def convert_index_to_value(index, invert=False):
+        ratio = convert_index_to_ratio(index)
+        delta = SCALE_MAX_Y_VALUE - SCALE_MIN_Y_VALUE
+
+        if invert:
+            return SCALE_MAX_Y_VALUE - (delta * ratio)
+        else:
+            return SCALE_MIN_Y_VALUE + (delta * ratio)
+
     # plot it
     if show_plots:
+        x = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, len(plot))
+        tickcount = round(np.ceil(abs(SCALE_MAX_Y_VALUE - SCALE_MIN_Y_VALUE) + 1))
+        labels = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, tickcount, dtype=int)
         averaged_strip = next_axs()
+        averaged_strip.plot(x, plot)
         averaged_strip.set_title('Averaged Strip')
-        averaged_strip.plot(plot)
+        averaged_strip.set_xlim(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE)
+        averaged_strip.set_xticks(labels)  # add ticks
+        averaged_strip.set_xticklabels(labels)  # add ticks
 
     # convert plot to float32 (better gaussian blur results)
     plot = np.float32(plot)
@@ -114,18 +141,30 @@ def calculate_tds(input, reference, show_plots=True, write_images=True, debug_en
 
     # plot it
     if show_plots:
+        x = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, len(plot))
+        tickcount = round(np.ceil(abs(SCALE_MAX_Y_VALUE - SCALE_MIN_Y_VALUE) + 1))
+        labels = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, tickcount, dtype=int)
         raw = next_axs()
-        raw.plot(plot)
+        raw.plot(x, plot)
         raw.set_title('Smoothed')
+        raw.set_xlim(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE)
+        raw.set_xticks(labels)  # add ticks
+        raw.set_xticklabels(labels)  # add ticks
 
     # derivative
     plot2 = np.diff(plot)
 
     # plot it
     if show_plots:
+        x = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, len(plot2))
+        tickcount = round(np.ceil(abs(SCALE_MAX_Y_VALUE - SCALE_MIN_Y_VALUE) + 1))
+        labels = np.linspace(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE, tickcount, dtype=int)
         derivative = next_axs()
-        derivative.plot(plot2)
+        derivative.plot(x, plot2)
         derivative.set_title('Derivative')
+        derivative.set_xlim(SCALE_MIN_Y_VALUE, SCALE_MAX_Y_VALUE)
+        derivative.set_xticks(labels)  # add ticks
+        derivative.set_xticklabels(labels)  # add ticks
 
     def find_nearest(array, value):
         array = np.asarray(array)
@@ -148,19 +187,21 @@ def calculate_tds(input, reference, show_plots=True, write_images=True, debug_en
     via_argmax = plot2.argmax()
     via_middle = halfcross
 
-    def convert_index_to_tds(index):
-        ratio = index / len(plot)
-
-        return 1 - ratio
-
-    debug("tds (argmax)", convert_index_to_tds(via_argmax))
-    debug("tds (middle)", convert_index_to_tds(via_middle))
+    debug("value (argmax)", convert_index_to_ratio(via_argmax))
+    debug("value (middle)", convert_index_to_ratio(via_middle))
 
     # draw vertical line on all axs
     if show_plots:
         for ax in axs:
-            ax.axvline(x=via_argmax, color='r', linestyle='--')
-            ax.axvline(x=via_middle, color='g', linestyle='--')
+            via_argmax_value = convert_index_to_value(via_argmax)
+            via_argmax_label = 'argmax: {:.2f}'.format(via_argmax_value)
+            ax.axvline(x=via_argmax_value, color='r', linestyle='--', label=via_argmax_label)
+
+            via_middle_value = convert_index_to_value(via_middle)
+            via_middle_label = 'middle: {:.2f}'.format(via_middle_value)
+            ax.axvline(x=via_middle_value, color='b', linestyle='--', label=via_middle_label)
+
+            ax.legend()
 
         # remove x-axis margins
         for ax in axs:
@@ -168,19 +209,23 @@ def calculate_tds(input, reference, show_plots=True, write_images=True, debug_en
 
         plt.show()
 
-    return convert_index_to_tds(via_argmax), convert_index_to_tds(via_middle)
+    return convert_index_to_ratio(via_argmax), convert_index_to_ratio(via_middle)
 
 
 if __name__ == '__main__':
     images = [
-        'in.jpeg',
-        'in2.jpeg',
-        'in3.jpeg',
-        'inz.jpeg',
+        # 'in.jpeg',
+        # 'in2.jpeg',
+        # 'in3.jpeg',
+        # 'inz.jpeg',
+        'beer3_afterfermenting1.jpg',
+        'beer3_afterfermenting2.jpg',
+        'beer3_afterfermenting3.jpg',
+        'beer3_afterfermenting4.jpg',
     ]
 
     for image in images:
-        tds1, tds2 = calculate_tds(image, 'crop.jpeg', show_plots=False, write_images=False, debug_enabled=False)
+        argmax, middle = calculate_value(image, 'crop.jpeg', show_plots=True, write_images=False, debug_enabled=True)
 
         # print tds with 2 decimal places
-        print(f'{image}: {tds1:.2f} | {tds2:.2f}')
+        print(f'{image}: argmax={argmax:.2f} | middle={middle:.2f}')
